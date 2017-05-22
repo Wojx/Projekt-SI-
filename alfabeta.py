5c57c9f3f8d6296daa76cc2f11f386a19c47db6f
Wojciech Janowski <wojtek.janowski@yahoo.pl>
2017-05-22 17:23:37 +0200

    add a minimax and alphabeta implementation

--- alfabeta.py
+++ alfabeta.py
@@ -2,11 +2,11 @@ MAX_DEEP = 1000
 INFINITY = float('inf')
 
 ################################################
-#              Classic MinMaxTree class        #
+#              Classic AlphaBetaTree class        #
 ################################################
 
 
-class MinMaxAI:
+class AlphaBetaAI:
 
     def __init__(self, game_tree):
         self.game_tree = game_tree
@@ -35,38 +35,40 @@ class MinMaxAI:
         assert node is not None
         return node.deep
 
-    # max state
-    def max_value(self, node, deep):
+    # max alfa state
+    def max_value(self, node, deep, alpha, beta):
         if self.is_game_over(node) or deep == 0:
             return self.get_value(node)
-
-        maximum_value = -INFINITY
+        max_value = alpha
         successors = self.get_children(node)
         for state in successors:
-            maximum_value = max(maximum_value, self.min_value(state, deep-1))
-        return maximum_value
+            max_value = max(max_value, self.min_value(state, deep-1, max_value, beta))
+            if max_value >= beta:
+                break
+        return max_value
 
-    # min state
-    def min_value(self, node, deep):
+    # min beta state
+    def min_value(self, node, deep, alpha, beta):
         if self.is_game_over(node) or deep == 0:
             return self.get_value(node)
-
-        minimum_value = INFINITY
+        min_value = beta
         successors = self.get_children(node)
         for state in successors:
-            minimum_value = max(minimum_value, self.max_value(state, deep-1))
-        return minimum_value
+            min_value = min(min_value, self.max_value(state, deep-1, alpha, min_value))
+            if alpha >= min_value:
+                break
+        return min_value
 
     # initial method
     def start(self, node):
-        best_value = self.max_value(self, node, MAX_DEEP)
+        best_value = -INFINITY
+
         successors = self.get_children(node)
         best_move = None
         for elem in successors:
-            if elem.value == best_value:
+            value = self.min_value(elem, MAX_DEEP, best_value, INFINITY)
+            if value > best_value:
+                best_value = value
                 best_move = elem
-                break
 
         return best_move
-
-
